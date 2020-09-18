c9d40b9c169a0747cf49fcef61cc52ba311bdd2c
AbhishekS <abhishek.shere09@gmail.com>
2020-09-19 00:17:32 +0530

    Create movie and show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,32 @@
 class MoviesController < ApplicationController
+  # before_action :authorized, only: [:new, :create]
+
+  def index
+    @user = User.new unless logged_in?
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = current_user.movies.new(movie_params)
+
+    if @movie.save!
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:url, :title, :description)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,19 @@
 class Movie < ApplicationRecord
+  # Association
   belongs_to :user
-
+  # Validation
   validates :url, :title, presence: true
+  # Callbacks
+  before_save :embed_url
+
+  def embed_url
+    if self.url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      self.url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+
+    self.url = "http://www.youtube.com/embed/#{ youtube_id }"
+  end
 end
