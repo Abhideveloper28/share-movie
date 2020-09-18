dfe418e7f15fc3c39ceb7523dca45e0f541eb4f0
AbhishekS <abhishek.shere09@gmail.com>
2020-09-18 23:11:35 +0530

    Add movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_191233) do
+ActiveRecord::Schema.define(version: 2020_09_18_173146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "url"
+    t.string "title"
+    t.string "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_09_17_191233) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "movies", "users"
 end
