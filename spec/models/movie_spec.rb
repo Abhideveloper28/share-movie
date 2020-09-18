0f5beae64adecbc0bad3fa7427724fea211488e7
AbhishekS <abhishek.shere09@gmail.com>
2020-09-19 01:34:46 +0530

    Add model spec

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(url: "https://www.youtube.com/watch?v=GY7Ps8fqGdc",
+                        title: "Ruby on Rails Tutorial",
+                        description: "For quite some time I have been using Rails and NodeJS for all of my web and Android development so I decided to make a Ruby on Rails video tutorial. I will cover a lot in this tutorial. We'll cover installation, MVC, setting up MySQL, the file system, creating controllers and views, embedded Ruby, Routes, communication between the controller and views, migrations and more.")
+  }
+
+  it "is not valid without a url" do
+    subject.url = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a url" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
 end
